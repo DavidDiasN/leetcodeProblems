2b2070eaaefa7f64e74efe3a2c952b6692419381
David Dias <daviddiasnogueira@gmail.com>
2024-02-26 14:24:45 -0500

    reverse LL ez

---
+++ reverseLL/main.go
@@ -0,0 +1,29 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	p1 := head
+	p2 := head
+
+	savep2Next := p2.Next
+
+	for p2 != nil {
+		savep2Next = p2.Next
+		if p2 == head {
+			p2.Next = nil
+		} else {
+			p2.Next = p1
+		}
+		p1 = p2
+		p2 = savep2Next
+	}
+	return p1
+
+}
