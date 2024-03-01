8398efc2f4fa8d9759429a0ceef7c332c00a7a99
David Dias <daviddiasnogueira@gmail.com>
2024-03-01 00:52:48 -0500

    I am sick so this one problem took me all day...Brain hurty

---
+++ rotateImage/main.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+}
+
+func rotate(matrix [][]int) {
+	l, r := 0, len(matrix)-1
+
+	for l < r {
+		for i := 0; i < r-l; i++ {
+			top, bottom := l, r
+			topLeft := matrix[top][l+i]
+			matrix[top][l+i] = matrix[bottom-i][l]
+			matrix[bottom-i][l] = matrix[bottom][r-i]
+			matrix[bottom][r-i] = matrix[top+i][r]
+			matrix[top+i][r] = topLeft
+		}
+		r--
+		l++
+
+	}
+
+}
