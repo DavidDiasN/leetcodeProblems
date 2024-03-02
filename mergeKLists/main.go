7d64a4f458a8a169666d7febb8a9033693af4d78
David Dias <daviddiasnogueira@gmail.com>
2024-03-01 20:54:58 -0500

    Wow that solution runs so much better

--- mergeKLists/main.go
+++ mergeKLists/main.go
@@ -61,3 +61,64 @@ func mergeKLists(lists []*ListNode) *ListNode {
 func remove(slice []*ListNode, s int) []*ListNode {
 	return append(slice[:s], slice[s+1:]...)
 }
+
+
+ func neetMergeKLists(lists []*ListNode) *ListNode {
+	if lists == nil || len(lists) == 0 {
+			return nil
+	}
+
+	for len(lists) > 1 {
+			mergedLists := []*ListNode{}
+			for i := 0; i < len(lists); i+=2 {
+					l1 := lists[i]
+					var l2 *ListNode
+					if i+1 <= len(lists)-1 {
+							l2 = lists[i+1]
+							mergedLists = append(mergedLists, mergeLists(l1, l2))
+					} else {
+							mergedLists = append(mergedLists, l1)
+					}
+			}
+			lists = mergedLists
+	}
+	return lists[0]
+
+}
+
+func mergeLists(l1, l2 *ListNode) *ListNode {
+	if l1 == nil {
+			return l2
+	} else if l2 == nil {
+			return l1
+	}
+	dummy := &ListNode{}
+	tail := dummy
+
+	for l1 != nil || l2 != nil {
+			if l1 == nil {
+					dummy.Next = l2
+					break
+			} else if l2 == nil {
+					dummy.Next = l1
+					break
+			}
+
+			if l1.Val < l2.Val {
+					dummy.Val = l1.Val
+					l1 = l1.Next
+			} else {
+					dummy.Val = l2.Val
+					l2 = l2.Next
+			}
+
+			if l1 == nil || l2 == nil {
+					continue
+			}
+			nextDummy := &ListNode{}
+			dummy.Next = nextDummy
+			dummy = dummy.Next
+	}
+
+	return tail
+}
